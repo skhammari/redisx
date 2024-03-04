800871979365542c45d85bc43d609c6d2e800044
skhammari <skhammari@gmail.com>
2024-03-05 00:18:41 +0330

    change boot method

--- redisx.go
+++ redisx.go
@@ -1,14 +1,33 @@
 package redisx
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
 
-type RedisX struct {
-	client *redis.Client
+var (
+	redisClient *redis.Client
+)
+
+func initDefaultClient(options *redis.Options) {
+	redisClient = redis.NewClient(options)
 }
 
-func NewRedisX(options *redis.Options) *RedisX {
-	client := redis.NewClient(options)
-	return &RedisX{
-		client: client,
+func getDefaultClient() *redis.Client {
+	return redisClient
+}
+
+func RedisX() *redis.Client {
+	redisOptions := &redis.Options{}
+	initDefaultClient(redisOptions)
+	client := getDefaultClient()
+	_, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Println("Error connecting to Redis: ", err)
+	} else {
+		fmt.Println("Connected to Redis")
 	}
+
+	return client
 }
